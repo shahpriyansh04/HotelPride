0dd8e416fd7ec516de4e56a3d51c12ba457726a1
Priyansh <shah.priyansh04@gmail.com>
2024-03-03 13:11:10 +0530

    location overflow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className=" flex justify-center py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
+    <footer className="flex justify-center py-6 w-full  items-center px-4 md:px-6 border-t">
       <p className="text-xs text-gray-500 dark:text-gray-400">
         © Hotel Pride. All rights reserved.
       </p>
